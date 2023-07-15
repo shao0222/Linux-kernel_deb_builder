6ad4d8e3d24637fdacfcd8ccebf2a2d726f818fa
XXTXTOP <xxtxtop@gmail.com>
2023-07-15 16:27:46 +0800

    Update get_latest_kernel.py

--- get_latest_kernel.py
+++ get_latest_kernel.py
@@ -53,14 +53,25 @@ def get_latest_kernel():
         elif latest_kernel <= current_kernel: # 如果最新的内核版本等于或小于当前的内核版本
             latest_kernel_label.config(text=f"当前的内核版本已经是最新的", fg="green") # 更新标签上的文本，用绿色字体
 
+def execute_script():
+    try:
+        os.system("./script.sh")  # Execute the script.sh file
+        print("script.sh 执行成功")
+    except Exception as e:
+        print(f"执行 script.sh 失败：{e}")
+
 # 创建一个窗口
 window = tk.Tk()
 window.title("获取最新的Linux内核版本") # 设置窗口标题
-window.geometry("400x200") # 设置窗口大小
+window.geometry("400x250") # 设置窗口大小
 
 # 创建一个按钮，点击时调用get_latest_kernel函数
-button = tk.Button(window, text="获取最新的内核版本", command=get_latest_kernel)
-button.pack() # 将按钮添加到窗口中
+button1 = tk.Button(window, text="获取最新的内核版本", command=get_latest_kernel)
+button1.pack() # 将按钮添加到窗口中
+
+# 创建一个按钮，点击时执行 script.sh 脚本
+button2 = tk.Button(window, text="执行 script.sh", command=execute_script)
+button2.pack() # 将按钮添加到窗口中
 
 # 创建两个标签，用于显示最新的内核版本和当前的内核版本
 latest_kernel_label = tk.Label(window, text="最新的Linux内核版本是：")
