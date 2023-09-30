dbdc4ff189ed07e21fbe9dcd74268a6708ca2913
XXTXTOP <xxtxtop@gmail.com>
2023-10-01 00:36:02 +0800

    精简代码2

--- build_action.sh
+++ build_action.sh
@@ -44,30 +44,8 @@ cd linux-"$mainline" || exit
 
 # copy config file
 cp ../config .config
-#
-# disable DEBUG_INFO to speedup build
-# scripts/config --disable DEBUG_INFO 
-scripts/config --disable DEBUG_INFO_X86
-scripts/config --disable DEBUG_INFO_VMCORE
-scripts/config --disable DEBUG_INFO_SPLIT
-scripts/config --disable DEBUG_INFO_BTF_MODULES
-scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
-scripts/config --disable DEBUG_INFO_PERF
-scripts/config --disable DEBUG_INFO_BTF
-scripts/config --disable DEBUG_INFO_DWARF4
-scripts/config --disable DEBUG_INFO_REDUCED
-scripts/config --set-str SYSTEM_TRUSTED_KEYS "" 
-scripts/config --set-str SYSTEM_REVOCATION_KEYS "" 
-scripts/config --undefine DEBUG_INFO 
-scripts/config --undefine DEBUG_INFO_COMPRESSED 
-scripts/config --undefine DEBUG_INFO_REDUCED 
-scripts/config --undefine DEBUG_INFO_SPLIT 
-scripts/config --undefine GDB_SCRIPTS 
-scripts/config --set-val DEBUG_INFO_DWARF5 n 
-scripts/config --set-val DEBUG_INFO_NONE y 
-# apply patches
-# shellcheck source=src/util.sh
-# source ../patch.d/*.sh
+
+sudo bash config_setup.sh
 
 
 
@@ -104,27 +82,8 @@ cd linux-"$stable" || exit
 
 # copy config file
 cp ../config .config
-#
-# disable DEBUG_INFO to speedup build
-# scripts/config --disable DEBUG_INFO 
-scripts/config --disable DEBUG_INFO_X86
-scripts/config --disable DEBUG_INFO_VMCORE
-scripts/config --disable DEBUG_INFO_SPLIT
-scripts/config --disable DEBUG_INFO_BTF_MODULES
-scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
-scripts/config --disable DEBUG_INFO_PERF
-scripts/config --disable DEBUG_INFO_BTF
-scripts/config --disable DEBUG_INFO_DWARF4
-scripts/config --disable DEBUG_INFO_REDUCED
-scripts/config --set-str SYSTEM_TRUSTED_KEYS "" 
-scripts/config --set-str SYSTEM_REVOCATION_KEYS "" 
-scripts/config --undefine DEBUG_INFO 
-scripts/config --undefine DEBUG_INFO_COMPRESSED 
-scripts/config --undefine DEBUG_INFO_REDUCED 
-scripts/config --undefine DEBUG_INFO_SPLIT 
-scripts/config --undefine GDB_SCRIPTS 
-scripts/config --set-val DEBUG_INFO_DWARF5 n 
-scripts/config --set-val DEBUG_INFO_NONE y 
+
+sudo bash config_setup.sh
 
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
@@ -159,27 +118,7 @@ cd linux-"$longterm" || exit
 
 # copy config file
 cp ../config .config
-#
-# disable DEBUG_INFO to speedup build
-# scripts/config --disable DEBUG_INFO 
-scripts/config --disable DEBUG_INFO_X86
-scripts/config --disable DEBUG_INFO_VMCORE
-scripts/config --disable DEBUG_INFO_SPLIT
-scripts/config --disable DEBUG_INFO_BTF_MODULES
-scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
-scripts/config --disable DEBUG_INFO_PERF
-scripts/config --disable DEBUG_INFO_BTF
-scripts/config --disable DEBUG_INFO_DWARF4
-scripts/config --disable DEBUG_INFO_REDUCED
-scripts/config --set-str SYSTEM_TRUSTED_KEYS "" 
-scripts/config --set-str SYSTEM_REVOCATION_KEYS "" 
-scripts/config --undefine DEBUG_INFO 
-scripts/config --undefine DEBUG_INFO_COMPRESSED 
-scripts/config --undefine DEBUG_INFO_REDUCED 
-scripts/config --undefine DEBUG_INFO_SPLIT 
-scripts/config --undefine GDB_SCRIPTS 
-scripts/config --set-val DEBUG_INFO_DWARF5 n 
-scripts/config --set-val DEBUG_INFO_NONE y 
+sudo bash config_setup.sh
 
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
@@ -187,7 +126,6 @@ sudo make bindeb-pkg -j"$CPU_CORES"
 
 # move deb packages to artifact dir
 cd ..
-
 
 mkdir "artifact"
 mkdir kernel/$SHOWVERSION
---
+++ config_setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#利用scripts/config对内核进行修改，之后需要写个注释对上述提到的所以东西进行讲解
+scripts/config --disable DEBUG_INFO_X86
+scripts/config --disable DEBUG_INFO_VMCORE
+scripts/config --disable DEBUG_INFO_SPLIT
+scripts/config --disable DEBUG_INFO_BTF_MODULES
+scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
+scripts/config --disable DEBUG_INFO_PERF
+scripts/config --disable DEBUG_INFO_BTF
+scripts/config --disable DEBUG_INFO_DWARF4
+scripts/config --disable DEBUG_INFO_REDUCED
+scripts/config --set-str SYSTEM_TRUSTED_KEYS "" 
+scripts/config --set-str SYSTEM_REVOCATION_KEYS "" 
+scripts/config --undefine DEBUG_INFO 
+scripts/config --undefine DEBUG_INFO_COMPRESSED 
+scripts/config --undefine DEBUG_INFO_REDUCED 
+scripts/config --undefine DEBUG_INFO_SPLIT 
+scripts/config --undefine GDB_SCRIPTS 
+scripts/config --set-val DEBUG_INFO_DWARF5 n 
+scripts/config --set-val DEBUG_INFO_NONE y 
+
+# 支持2.4G无线键盘
+# <*> USB HID transport layer
+scripts/config -e CONFIG_USB_HID
+# 提高桌面响应速度
+# (X) Preemptible Kernel (Low-Latency Desktop) [CONFIG_PREEMPT]
+scripts/config -e CONFIG_PREEMPT
+# (X) Idle dynticks system (tickless idle) [CONFIG_NO_HZ_IDLE]
+scripts/config -e CONFIG_NO_HZ_IDLE
+# (X) Simple tick based cputime accounting [CONFIG_TICK_CPU_ACCOUNTING]
+scripts/config -e CONFIG_TICK_CPU_ACCOUNTING
+# 桌面系统的“鸡血补丁”，开启后能显著降低操作延迟、提升程序响应速度
+# [*] Automatic process group schedulin [CONFIG_SCHED_AUTOGROUP]
+scripts/config -e CONFIG_SCHED_AUTOGROUP
